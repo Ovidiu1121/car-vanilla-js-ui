78b5d93c59a0c85cdc75cf87b45ecc537f0e9315
Ovidiu Gusteritan <gusteritanovidiu@gmail.com>
2024-09-02 16:24:25 +0300

    done alerts

--- functions.js
+++ functions.js
@@ -1,10 +1,13 @@
-export function createHome() {
+export function createHome(alert) {
 
     let container = document.querySelector(".container");
 
 
     container.innerHTML = `
-    
+     <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+       </div>  
+
     	<h1>Cars</h1>
 
     <button class="button">Add car</button>
@@ -23,22 +26,81 @@ export function createHome() {
 		</tbody>
 	</table>
     `
+    let button = document.querySelector(".button");
+    let table = document.querySelector(".table");
+    const alertPlaceholder = document.querySelector('.container-alert');
+    let load = document.querySelector(".spinner-border");
+
+    const appendAlert = (message, type) => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+            `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+            `   <div>${message}</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+        ].join('')
+
+        alertPlaceholder.append(wrapper)
+    }
 
     api("https://localhost:7164/api/v1/Car/all").then(response => {
         return response.json();
     }).then(data => {
-        console.log(data);
+        load.classList = "";
+        //console.log(data);
         attachCars(data.carList);
+
     }).catch(error => {
+
+        load.classList = "";
+
         console.error('Error fetching data:', error);
+
+        appendAlert(error, "danger");
     });
-
-
-    let button = document.querySelector(".button");
 
     button.addEventListener("click", (eve) => {
         CreateAddCarPage();
     });
+
+    table.addEventListener("click", (eve) => {
+
+        if (eve.target.classList.contains("updateCar")) {
+            api(`https://localhost:7164/api/v1/Car/id/${eve.target.textContent}`).then(res => {
+                return res.json();
+            }).then(data => {
+                console.log(data);
+
+                let car = {
+                    brand: data.brand,
+                    price: data.price,
+                    horse_power: data.horse_power,
+                    fabrication_year: data.fabrication_year
+                }
+
+                CreateUpdatePage(car, eve.target.textContent);
+
+            }).catch(error => {
+                console.error('Error fetching data:', error);
+            });
+        }
+
+    });
+
+    if (alert === "deleted") {
+        load.classList = "";
+        appendAlert("Car has been DELETED with success!", "success");
+    }
+
+    if (alert === "updated") {
+        load.classList = "";
+        appendAlert("Car has been UPDATED with success!", "success");
+    }
+
+    if (alert === "added") {
+        load.classList = "";
+        appendAlert("Car has been ADDED with success!", "success");
+    }
 
 }
 
@@ -85,12 +147,91 @@ export function CreateAddCarPage() {
     let test = document.querySelector(".createCar");
 
     button.addEventListener("click", (eve) => {
-        createHome();
+        createHome("");
     })
 
     test.addEventListener("click", (eve) => {
-        createCar();
+        createUpdateCar("create");
     })
+
+}
+
+export function CreateUpdatePage(car, idCar) {
+
+    let container = document.querySelector(".container");
+
+    container.innerHTML = `
+    <h1>Update Car</h1>
+    <form>
+        <p>
+            <label for="brand">Brand</label>
+            <input name="brand" type="text" id="brand" value="${car.brand}">
+             <a class="brandErr">Brand required!</a>
+        </p>
+         <p >
+             <label for="fabricationYear">Fabrication Year</label>
+            <input name="fabricationYear" type="text" id="fabricationYear" value="${car.fabrication_year}">
+             <a class="fabricationYearErr">Fabrication Year required!</a>
+        </p>
+        <p>
+            <label for="price">Price</label>
+            <input name="price" type="text" id="price" value="${car.price}">
+             <a class="priceErr">Price required!</a>
+        </p>
+        <p>
+            <label for="horsePower">Horse Power</label>
+            <input name="horsePower" type="text" id="horsePower" value="${car.horse_power}">
+             <a class="horsePowerErr">Horse Power required!</a>
+        </p>
+
+
+        <div class="submitUpdate">
+         <a href="#">Update Car</a>
+        </div>
+
+          <div class="cancel">
+         <a href="#">Cancel</a>
+        </div>
+        <div class="submitDelete">
+         <a href="#">Delete Car</a>
+        </div>
+    </form>
+    `
+
+    let cancelButton = document.querySelector(".cancel");
+    let submitUpdateButton = document.querySelector(".submitUpdate");
+    let submitDeleteButton = document.querySelector(".submitDelete");
+    let brandinput = document.getElementById("brand");
+    let fabricationyearinput = document.getElementById("fabricationYear");
+
+    brandinput.disabled = true;
+    fabricationyearinput.disabled = true;
+
+    cancelButton.addEventListener("click", (eve) => {
+        createHome("");
+    });
+
+    submitUpdateButton.addEventListener("click", (eve) => {
+        createUpdateCar("update", idCar);
+    });
+
+    submitDeleteButton.addEventListener("click", (eve) => {
+
+        api(`https://localhost:7164/api/v1/Car/delete/${idCar}`, "DELETE")
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+
+                createHome("deleted");
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+
+    })
+
 
 }
 
@@ -99,7 +240,7 @@ function createRow(car) {
     let tr = document.createElement("tr");
 
     tr.innerHTML = `
-				<td>${car.id}</td>
+				<td  class="updateCar">${car.id}</td>
 				<td>${car.price}</td>
 				<td>${car.brand}</td>
 				<td>${car.horse_power}</td>
@@ -141,7 +282,7 @@ function attachCars(cars) {
 
 }
 
-function createCar() {
+function createUpdateCar(request, idCar) {
 
     const isNumber = (str) => {
         return /^[+-]?\d+(\.\d+)?$/.test(str);
@@ -183,6 +324,7 @@ function createCar() {
 
         errors.push("HorsePower");
 
+
     } else if (horsePowerError.classList.contains("beDisplayed") && horsePower !== '') {
 
         errors.pop("HorsePower");
@@ -200,8 +342,6 @@ function createCar() {
         fabricationYearError.classList.remove("beDisplayed");
 
     }
-
-    console.log(horsePower.classList);
 
     if (!isNumber(horsePower) && horsePower != '') {
 
@@ -241,19 +381,31 @@ function createCar() {
             fabrication_year: fabricationYear
         }
 
-        console.log(car);
-
-        api("https://localhost:7164/api/v1/Car/create", "POST", car)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-                createHome();
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
+        if (request === "create") {
+            api("https://localhost:7164/api/v1/Car/create", "POST", car)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    createHome("added");
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        } else if (request === "update") {
+            api(`https://localhost:7164/api/v1/Car/update/${idCar}`, "PUT", car)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    createHome("updated");
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
     } else {
 
         errors.forEach(err => {
